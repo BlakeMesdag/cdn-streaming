24f925dec5c2080ad09568f3f94bf3f3bf28a0a0
Blake Mesdag <blakemesdag@gmail.com>
2014-11-14 13:36:54 -0500

    Use a streaming log to warm a cache

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"github.com/ActiveState/tail"
+	"net/http"
+)
+
+func HitUrl(url string) (byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Sprintf("Error getting %s: %s", url, err)
+	}
+
+	defer resp.Body.Close()
+	r := bufio.NewReader(resp.Body)
+	if err != nil {
+		fmt.Sprintln("Error initialize scanner for %s: %s", url, err)
+	}
+
+	return r.ReadByte()
+}
+
+func main() {
+	logFilePath := flag.String("file", "varnish-urls.log", "location of log of urls to follow")
+	flag.Parse()
+
+	fmt.Println("Beginning to follow file: ", *logFilePath)
+
+	offset := tail.SeekInfo{Offset: 0, Whence: 2}
+	t, err := tail.TailFile(*logFilePath, tail.Config{Follow: true, Location: &offset})
+	if err != nil {
+		fmt.Sprintln("Error encountered opening file: %s", err)
+	}
+
+	// defer t.Stop()
+
+	for url := range t.Lines {
+		fmt.Println("Hitting url: ", url.Text)
+		go HitUrl(url.Text)
+	}
+}
