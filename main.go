3b0d16f53acc50723e4569179aae1d3e763dc3f2
Blake Mesdag <blakemesdag@gmail.com>
2014-11-14 13:40:32 -0500

    Be consistent while printing

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ import (
 func HitUrl(url string) (byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Sprintf("Error getting %s: %s", url, err)
+		fmt.Println(fmt.Sprintf("Error getting %s: %s", url, err))
 	}
 
 	defer resp.Body.Close()
 	r := bufio.NewReader(resp.Body)
 	if err != nil {
-		fmt.Sprintln("Error initialize scanner for %s: %s", url, err)
+		fmt.Println(fmt.Sprintf("Error initialize scanner for %s: %s", url, err))
 	}
 
 	return r.ReadByte()
@@ -27,18 +27,18 @@ func main() {
 	logFilePath := flag.String("file", "varnish-urls.log", "location of log of urls to follow")
 	flag.Parse()
 
-	fmt.Println("Beginning to follow file: ", *logFilePath)
+	fmt.Println(fmt.Sprintf("Beginning to follow file: %s", *logFilePath))
 
 	offset := tail.SeekInfo{Offset: 0, Whence: 2}
 	t, err := tail.TailFile(*logFilePath, tail.Config{Follow: true, Location: &offset})
 	if err != nil {
-		fmt.Sprintln("Error encountered opening file: %s", err)
+		fmt.Println(fmt.Sprintf("Error encountered opening file: %s", err))
 	}
 
 	// defer t.Stop()
 
 	for url := range t.Lines {
-		fmt.Println("Hitting url: ", url.Text)
+		fmt.Println(fmt.Sprintf("Hitting url: %s", url.Text))
 		go HitUrl(url.Text)
 	}
 }
